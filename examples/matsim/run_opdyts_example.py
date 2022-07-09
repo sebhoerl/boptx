9ab1d342c35d7e8981c56b50528e389c9798b3f4
Sebastian Hörl <hoerl.sebastian@gmail.com>
2022-07-09 15:28:53 +0200

    update

--- examples/matsim/run_opdyts_example.py
+++ examples/matsim/run_opdyts_example.py
@@ -3,7 +3,7 @@ from boptx.problem import ContinuousParameter
 from boptx.algorithms import CMAES1P1Algorithm, OpdytsAlgorithm
 import time, os, shutil
 import logging
-logging.basicConfig(level = logging.INFO)
+logging.basicConfig(level = logging.DEBUG)
 
 # git clone https://github.com/matsim-scenarios/matsim-berlin
 # ./mvnw clean package -DskipTests=true
